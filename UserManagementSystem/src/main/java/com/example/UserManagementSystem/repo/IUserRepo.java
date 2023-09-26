2b305d1b40fe466d794ed45ec98c3ddf003445af
Manjeet Kumar Gautam <86048496+ManjeetKumarGautam@users.noreply.github.com>
2023-09-26 15:05:51 +0530

    Delete UserManagementSystem directory

--- UserManagementSystem/src/main/java/com/example/UserManagementSystem/repo/IUserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.UserManagementSystem.repo;
-
-import com.example.UserManagementSystem.model.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IUserRepo extends CrudRepository <User,Integer>{
-}
