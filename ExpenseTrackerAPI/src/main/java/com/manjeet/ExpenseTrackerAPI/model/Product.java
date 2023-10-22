4d461189385e970d79dbeb58aca3209c46e8a9b1
kakarot <manjeetkumar1572000@gmail.com>
2023-10-22 22:34:43 +0530

    Update

--- ExpenseTrackerAPI/src/main/java/com/manjeet/ExpenseTrackerAPI/model/Product.java
+++ ExpenseTrackerAPI/src/main/java/com/manjeet/ExpenseTrackerAPI/model/Product.java
@@ -1,9 +1,6 @@
 package com.manjeet.ExpenseTrackerAPI.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +14,7 @@ import java.time.LocalTime;
 @Entity
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
     private String description;
--- ExpenseTrackerAPI/src/main/java/com/manjeet/ExpenseTrackerAPI/model/User.java
+++ ExpenseTrackerAPI/src/main/java/com/manjeet/ExpenseTrackerAPI/model/User.java
@@ -1,7 +1,10 @@
 package com.manjeet.ExpenseTrackerAPI.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,8 +16,11 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String username;
+
+    @Email
     private String email;
     private String password;
 }
